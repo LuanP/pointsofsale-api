0dea60612fb05d259c61d1c2d6a601ee7bf2d75e
Luan Pablo <luanpab@gmail.com>
2017-10-27 07:02:39 -0200

    removed unused error handling functions

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -2,19 +2,12 @@
 
 const boom = require('boom')
 
-const errorSource = {
-  payload: 'payload',
-  params: 'params',
-  query: 'query'
-}
-
 const ResponseHandler = () => {}
 
 ResponseHandler.errorHandler = () => {}
-ResponseHandler.errorHandler.errorSource = errorSource
 
 /**
- * Method: Create an error object to throw, with luizalabs-commons pattern
+ * Method: Create an error object to throw, with pattern
  * @param {Object} error Error
  * @param {String} sourceValidation Type of validation, can be payload, path, query or undefined
  */
@@ -33,18 +26,6 @@ ResponseHandler.parser = (error, sourceValidation) => {
 }
 
 /**
- * Method: Create a record not found error object to raise
- * @param {String} message - Text to display that was not found
- */
-ResponseHandler.errorHandler.recordNotFound = (message) => {
-  const data = {
-    parameters: message
-  }
-
-  return boom.notFound('Record not found', data)
-}
-
-/**
  * Method: Create a resource not found error object to raise
  */
 ResponseHandler.errorHandler.resourceNotFound = () => {
@@ -53,27 +34,6 @@ ResponseHandler.errorHandler.resourceNotFound = () => {
   }
 
   return boom.notFound('Resource not found', data)
-}
-
-/**
- * Method: Create a bad request error object to raise
- * @param {Object} error - Error to raise
- * @param {String} source - Type of validation, can be payload, path, query or undefined
- */
-ResponseHandler.errorHandler.badRequest = (error, source) => {
-  let result = boom.badRequest('Bad request', error)
-  let parsedError = ResponseHandler.parser(result, source)
-
-  return parsedError
-}
-
-/**
- * Method: Create an internal server error object to be raised
- * @param {Object} message - Message to be displayed on error
- * @param {Object} error - Error to raise
- */
-ResponseHandler.errorHandler.internal = (message, error) => {
-  return boom.internal(message, error)
 }
 
 /**
